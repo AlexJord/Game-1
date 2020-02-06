22787c2a48552af5c19067c8564e239a2ff0f87c
AlexJord <ajordan@stcharlessd.org>
2020-02-06 12:52:53 -0600

    modified:   First Game.py

--- First Game.py
+++ First Game.py
@@ -4,29 +4,44 @@
 
 '''Building a game with Python'''
 
+from random import randint
 
-game_running = True 
+game_running = True
+game_results = []
 
+def calculate_monster_attack(attack_min, attack_max):
+    return randint(attack_min, attack_max)
+
+def game_ends(winner_name):
+    print(f'{winner_name} won the game! Do thou dare to play again?')
 
 while game_running == True:
+    counter = 0
     new_round = True
     player = {'name': 'Alex', 'attack': 13, 'heal': 16, 'health': 100}
-    monster = {'name': 'Brian', 'attack': 12, 'health': 100}  
+    monster = {'name': 'Brian', 'attack_min': 10, 'attack_max': 20, 'health': 100}
+    
     
     print('---' * 7)
-    player['name'] = input("Enter Player name ")
+    player['name'] = input("You dare to challenge the monster who hast brought danger upon us? Well then... enter your name here champion! ")
+    
+    print('---' * 7)
+    print(player['name'] + ' has ' + str(player['health']) + ' health')
+    print(monster['name'] + ' has ' + str(monster['health']) + ' health')
     
     while new_round == True:
-    
+        
+        counter = counter + 1
         player_won = False
         monster_won = False
         
     
-    print('---' * 7)
+        print('---' * 7)
         print('Please select action')
-        print('1) Attack')
-        print('2) Heal')
+        print('1)  Attack!')
+        print('2) Drink holy water')
         print('3) Exit Game')
+        print('4) Show Reslts')
         
         player_choice = str(input())
         
@@ -36,24 +51,48 @@ while game_running == True:
                 player_won = True
             
             else: 
-                player['health'] = player['health'] - monster['attack']
+                player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
                 if player['health'] <= 0:
                     monster_won = True
             
-            print(monster['health'])
-            print(player['health'])
-        
-            
         elif player_choice == '2':
-            print('Heal player')
+            player['health'] = player['health'] + player['heal']
+            
+            
+            player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
+            if player['health'] <= 0:
+                monster_won = True            
+            
+                        
+            
             
         elif player_choice == '3':
             new_round = False
             game_running = False
             
+        elif player_choice == '4':
+            for player_stat in game_results:
+                print(player_stat) 
+            
         else:
             print('Invalid Input')
+        
+        if player_won == False and monster_won == False:
+            print(player['name'] + ' has ' + str(player['health']) + ' left')
+            print(monster['name'] + ' has ' + str(monster['health']) + ' left')
             
-        if player_won == True or monster_won == True:
+        elif player_won:
+            game_ends(player['name'])
+            round_result = {'name': player['name'], 'health': player ['health'], 'rounds': counter}
+            game_results.append(round_result)
             new_round = False
-                
+            
+        elif monster_won:
+            game_ends(monster['name'])
+            round_result = {'name': player['name'], 'health': player ['health'], 'rounds': counter}
+            game_results.append(round_result)  
+            new_round = False
+            
+
+    
+            
